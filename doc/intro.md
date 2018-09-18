08dee37d3a4d5923f5f7be81f25e74aeb17846df
Aaron Diep <aaron@delite.ca>
2018-09-18 17:59:30 -0600

    deploy to clojars

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sqlite
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.clojars.cloggo/sqlite "0.1.0"
+  :description "FIXME: write description"
+  :url "https://github.com/cloggo/cljs-sqlite"
+  :license {:name "MIT" }
+  :source-paths ["src"]
+  :dependencies [[org.clojure/clojure "1.8.0"]])
